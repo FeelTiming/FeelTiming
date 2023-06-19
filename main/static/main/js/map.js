e330be96f06b0457148d4cceda1260000d8db0a2
Mumat0103 <wsx2138@naver.com>
2023-06-20 00:14:00 +0900

    fix : main.js 분기
    - main.js 분기

---
+++ main/static/main/js/map.js
@@ -0,0 +1,64 @@
+function geoFindMe() {
+  function kakao_map(latitude, longitude) {
+    var latitude;
+    var longitude;
+    if (latitude === null && longitude === null) {
+      latitude = 37.5760222;
+      longitude = 126.9769;
+    } else {
+      latitude = latitude;
+      longitude = longitude;
+    }
+
+    var container = document.getElementById("map");
+    var options_kakao = {
+      center: new kakao.maps.LatLng(latitude, longitude),
+      level: 3,
+    };
+    var map = new kakao.maps.Map(container, options_kakao);
+  }
+
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 15000,
+  };
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(position.coords.accuracy);
+    kakao_map(latitude, longitude);
+  }
+
+  function error(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        loc.innerHTML =
+          "이 문장은 사용자가 Geolocation API의 사용 요청을 거부했을 때 나타납니다!";
+        break;
+
+      case error.POSITION_UNAVAILABLE:
+        loc.innerHTML =
+          "이 문장은 가져온 위치 정보를 사용할 수 없을 때 나타납니다!";
+        break;
+
+      case error.TIMEOUT:
+        loc.innerHTML =
+          "이 문장은 위치 정보를 가져오기 위한 요청이 허용 시간을 초과했을 때 나타납니다!";
+        break;
+
+      case error.UNKNOWN_ERROR:
+        loc.innerHTML = "이 문장은 알 수 없는 오류가 발생했을 때 나타납니다!";
+        break;
+    }
+  }
+
+  if (!navigator.geolocation) {
+    alert("위치 정보 접근 불가");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+}
+
+geoFindMe();
