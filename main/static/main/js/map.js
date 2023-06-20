1141ab3e68de003ad54574828422e61a9ebc4f3f
mumat0103 <wsx2138@naver.com>
2023-06-20 17:36:20 +0900

    feat : navbar 로고 추가 및 하단 바 생성
    - navbar 로고 추가
    - navbar 아이콘 추가
    - 하단 바 생성

--- main/static/main/js/map.js
+++ main/static/main/js/map.js
@@ -1,64 +1,73 @@
 function geoFindMe() {
-  function kakao_map(latitude, longitude) {
-    var latitude;
-    var longitude;
-    if (latitude === null && longitude === null) {
-      latitude = 37.5760222;
-      longitude = 126.9769;
-    } else {
-      latitude = latitude;
-      longitude = longitude;
-    }
+	function kakao_map(latitude, longitude) {
+		var latitude;
+		var longitude;
+		if (latitude === null && longitude === null) {
+			latitude = 37.5760222;
+			longitude = 126.9769;
+		} else {
+			latitude = latitude;
+			longitude = longitude;
+		}
 
-    var container = document.getElementById("map");
-    var options_kakao = {
-      center: new kakao.maps.LatLng(latitude, longitude),
-      level: 3,
-    };
-    var map = new kakao.maps.Map(container, options_kakao);
-  }
+		var container = document.getElementById("map");
+		var options_kakao = {
+			center: new kakao.maps.LatLng(latitude, longitude),
+			level: 3,
+		};
+		var map = new kakao.maps.Map(container, options_kakao);
 
-  const options = {
-    enableHighAccuracy: true,
-    maximumAge: 30000,
-    timeout: 15000,
-  };
+		var imageSrc = "static/main/image/current.png", // 마커이미지의 주소입니다
+			imageSize = new kakao.maps.Size(20, 20); // 마커이미지의 크기입니다
 
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(position.coords.accuracy);
-    kakao_map(latitude, longitude);
-  }
+		var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-  function error(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        loc.innerHTML =
-          "이 문장은 사용자가 Geolocation API의 사용 요청을 거부했을 때 나타납니다!";
-        break;
+		var markerPosition = new kakao.maps.LatLng(latitude, longitude);
+		var marker = new kakao.maps.Marker({
+			position: markerPosition,
+			image: markerImage,
+		});
 
-      case error.POSITION_UNAVAILABLE:
-        loc.innerHTML =
-          "이 문장은 가져온 위치 정보를 사용할 수 없을 때 나타납니다!";
-        break;
+		marker.setMap(map);
+	}
 
-      case error.TIMEOUT:
-        loc.innerHTML =
-          "이 문장은 위치 정보를 가져오기 위한 요청이 허용 시간을 초과했을 때 나타납니다!";
-        break;
+	const options = {
+		enableHighAccuracy: true,
+		maximumAge: 30000,
+		timeout: 15000,
+	};
 
-      case error.UNKNOWN_ERROR:
-        loc.innerHTML = "이 문장은 알 수 없는 오류가 발생했을 때 나타납니다!";
-        break;
-    }
-  }
+	function success(position) {
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		kakao_map(latitude, longitude);
+	}
 
-  if (!navigator.geolocation) {
-    alert("위치 정보 접근 불가");
-  } else {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  }
+	function error(error) {
+		switch (error.code) {
+			case error.PERMISSION_DENIED:
+				loc.innerHTML = "이 문장은 사용자가 Geolocation API의 사용 요청을 거부했을 때 나타납니다!";
+				break;
+
+			case error.POSITION_UNAVAILABLE:
+				loc.innerHTML = "이 문장은 가져온 위치 정보를 사용할 수 없을 때 나타납니다!";
+				break;
+
+			case error.TIMEOUT:
+				loc.innerHTML = "이 문장은 위치 정보를 가져오기 위한 요청이 허용 시간을 초과했을 때 나타납니다!";
+				break;
+
+			case error.UNKNOWN_ERROR:
+				loc.innerHTML = "이 문장은 알 수 없는 오류가 발생했을 때 나타납니다!";
+				break;
+		}
+	}
+
+	if (!navigator.geolocation) {
+		alert("위치 정보 접근 불가");
+	} else {
+		navigator.geolocation.watchPosition(success, error, options);
+	}
 }
 
 geoFindMe();
