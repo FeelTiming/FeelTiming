0f8306151e4a24fb206d40df3fbabf80a52de01a
Mumat0103 <wsx2138@naver.com>
2023-06-25 14:13:42 +0900

    fix : 스태틱 경로

--- feeltiming/settings.py
+++ feeltiming/settings.py
@@ -90,11 +90,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/static/'
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
