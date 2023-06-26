6fbbf774b2720ffe610bf7789416d26807fcd40f
anj1nkyu <ajk465454@naver.com>
2023-06-26 17:45:00 +0900

    feat : csv upload

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class smoking_area(models.Model):
+    region= models.CharField(max_length=100, null=True)
+
+    facility_name= models.CharField(max_length=100, null=True)
+
+    facility_type= models.CharField(max_length=100, null=True)
+
+    location= models.CharField(max_length=100, null=True)
+
+    size= models.CharField(max_length=100, null=True)
+
+    date= models.CharField(max_length=100, null=True)
+
+    subject= models.CharField(max_length=100, null=True)
+
+    management_status= models.CharField(max_length=100, null=True)
+
+    management= models.CharField(max_length=100, null=True)
