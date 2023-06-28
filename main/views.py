a49df900a3860177bdfd298eead13be395c936aa
Cheon Sung Kyu <69793985+mumat0103@users.noreply.github.com>
2023-06-28 14:53:09 +0900

    Feat/map (#3)
    * fix : main.js 분기
    
    - main.js 분기
    
    * feat : navbar 로고 추가 및 하단 바 생성
    
    - navbar 로고 추가
    - navbar 아이콘 추가
    - 하단 바 생성
    
    * feat : 로드뷰 버튼 추가
    
    * feat : 흡연구역 마커 표시 기능
    
    * feat : 흡연구역 등록 버튼 추가
    
    * feat : 흡연 구역 표시 기능 추가
    
    * fix : 오류 수정
    
    * docs : requirements 수정
    
    * fix : error 메시지 추가
    
    * fix : settings.py 수정
    
    * fix : 스태틱 경로
    
    * feat : cors 헤더
    
    * docs : requirements 수정
    
    * fix : static 경로 변경
    
    * feat : 제보 모델 추가, csv 로더 함수화
    
    * feat : 흡연구역 인포 윈도우 추가
    
    흡연구역 정확한 위치를 인포 윈도우에 나타나게끔 구현
    
    * feat : 로드뷰, 길찾기 기능 추가

--- main/views.py
+++ main/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
-# Create your views here.
+import pandas as pd
+
+def enroll(dir):
+    data = pd.read_csv(f"static\main\data\{dir}.csv", encoding='cp949')
+
+    for i in range(len(data)):
+        gu = data.iloc[i, 0]
+        gubun = data.iloc[i, 1]
+        type = data.iloc[i, 2]
+        location = data.iloc[i, 3]
+        manage = data.iloc[i, 4]
+        smokingZone.objects.update_or_create(gu=gu, gubun=gubun, type=type, location=location, manage=manage)
+
 def main(request):
-    return render(request, 'main/main.html')
+    zone = smokingZone.objects.all()
+    if not zone:
+        print("등록된 데이터 없음.")
+        enroll("Seongbuk-gu")
+        redirect('/')
+    context = {'zone' : zone}
+    return render(request, 'main/main.html', context)
---
+++ static/main/js/map.js
@@ -0,0 +1,173 @@
+var map;
+var latitude;
+var longitude;
+
+function kakao_map(latitude, longitude) {
+	if (latitude === null && longitude === null) {
+		latitude = 37.5760222; // 광화문 좌표
+		longitude = 126.9769;
+	} else {
+		latitude = latitude;
+		longitude = longitude;
+	}
+
+	var container = document.getElementById("map");
+	var options_kakao = {
+		center: new kakao.maps.LatLng(latitude, longitude),
+		level: 3,
+	};
+	map = new kakao.maps.Map(container, options_kakao);
+
+	var imageSrc = "static/main/image/current.png", // 마커이미지의 주소입니다
+		imageSize = new kakao.maps.Size(20, 20); // 마커이미지의 크기입니다
+
+	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+	var markerPosition = new kakao.maps.LatLng(latitude, longitude);
+	var marker = new kakao.maps.Marker({
+		position: markerPosition,
+		image: markerImage,
+	});
+	marker.setMap(map);
+}
+
+function error(error) {
+	console.log(error);
+}
+
+function success(position) {
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+}
+
+const options = {
+	enableHighAccuracy: true,
+	maximumAge: 30000,
+	timeout: 15000,
+};
+
+function geoFindMe() {
+	function success(position) {
+		latitude = position.coords.latitude;
+		longitude = position.coords.longitude;
+		kakao_map(latitude, longitude);
+	}
+
+	if (!navigator.geolocation) {
+		alert("위치 정보 접근 불가");
+	} else {
+		navigator.geolocation.getCurrentPosition(success, error, options);
+	}
+}
+
+function setCenter() {
+	if (!navigator.geolocation) {
+		alert("위치 정보 접근 불가");
+	} else {
+		navigator.geolocation.watchPosition(success, error, options);
+		var moveLatLng = new kakao.maps.LatLng(latitude, longitude);
+		map.panTo(moveLatLng);
+	}
+}
+
+var clicked = true;
+function getLoadView() {
+	if (clicked) {
+		map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+		clicked = false;
+	} else {
+		map.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+		clicked = true;
+	}
+}
+
+// 인포윈도우
+var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+var coords = [];
+var description = [];
+async function findGeo(place) {
+	const apiKey = "96b121da45a7569503823a0f6fcfd41b";
+
+	for (var i = 0; i < place.length; i++) {
+		// 주소 정보 가져오는 api 쓰기
+		const keyword = place[i].location;
+		await fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${encodeURIComponent(keyword)}`, {
+			headers: {
+				Authorization: `KakaoAK ${apiKey}`,
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json(); // 응답 데이터를 JSON 형식으로 변환하여 반환
+				} else {
+					throw new Error("Error: " + response.status);
+				}
+			})
+			.then((data) => {
+				// 응답 데이터 처리
+				if (data.documents.length !== 0) {
+					var coord = new kakao.maps.LatLng(data.documents[0].y, data.documents[0].x);
+					coords.push(coord);
+					description.push({ desc: place[i].desc, id: data.documents[0].id });
+				}
+			})
+			.catch((error) => {
+				// 에러 처리
+				console.error(error);
+			});
+	}
+	displayMarker(coords, description);
+}
+
+// 지도에 마커를 표시하는 함수입니다
+async function displayMarker(coords, description) {
+	// 마커를 생성하고 지도에 표시합니다
+	for (var i = 0; i < coords.length; i++) {
+		var marker = await addMarker(coords[i]);
+		(
+			await function (marker, title) {
+				kakao.maps.event.addListener(marker, "click", function () {
+					var linkRoute = document.getElementById("link_route");
+					var linkRoadView = document.getElementById("link_roadView");
+					linkRoute.href = "https://map.kakao.com/link/to/" + title.id;
+					linkRoadView.href = "https://map.kakao.com/link/roadview/" + title.id;
+					displayInfowindow(marker, title.desc);
+				});
+				kakao.maps.event.addListener(map, "click", function () {
+					var map_container = document.getElementById("map");
+					var bar = document.getElementById("bottomBar");
+					map_container.style.height = "100%";
+					bar.style.height = "0%";
+					infowindow.close();
+				});
+			}
+		)(marker, description[i]);
+	}
+}
+
+function addMarker(position) {
+	var imageSrc = "static/main/image/smoke.svg", // 마커이미지의 주소입니다
+		imageSize = new kakao.maps.Size(24, 24), // 마커이미지의 크기입니다
+		markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+	var marker = new kakao.maps.Marker({
+		position: position, // 마커의 위치
+		image: markerImage,
+	});
+
+	marker.setMap(map); // 지도 위에 마커를 표출합니다
+
+	return marker;
+}
+
+function displayInfowindow(marker, title) {
+	var content = '<div class="info-window">' + "<p>" + title + "</p>" + "</div>";
+	infowindow.setContent(content);
+	var map_container = document.getElementById("map");
+	var bar = document.getElementById("bottomBar");
+	map_container.style.height = "70%";
+	bar.style.height = "30%";
+	infowindow.open(map, marker);
+}
+
+geoFindMe();
