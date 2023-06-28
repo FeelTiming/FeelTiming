6cad668caff3c5da7afe92207948dd72be1b4455
Cheon Sung Kyu <69793985+mumat0103@users.noreply.github.com>
2023-06-28 16:48:45 +0900

    Feat/map (#8)
    * fix : main.js 분기
    
    - main.js 분기
    
    * feat : navbar 로고 추가 및 하단 바 생성
    
    - navbar 로고 추가
    - navbar 아이콘 추가
    - 하단 바 생성
    
    * feat : 로드뷰 버튼 추가
    
    * feat : 흡연구역 마커 표시 기능
    
    * Feat/map (#2)
    
    * fix : main.js 분기
    
    - main.js 분기
    
    * feat : navbar 로고 추가 및 하단 바 생성
    
    - navbar 로고 추가
    - navbar 아이콘 추가
    - 하단 바 생성
    
    * feat : 로드뷰 버튼 추가
    
    * feat : 흡연구역 마커 표시 기능
    
    * feat : 흡연구역 등록 버튼 추가
    
    * feat : 흡연 구역 표시 기능 추가
    
    * fix : 오류 수정
    
    * docs : requirements 수정
    
    * fix : error 메시지 추가
    
    * fix : settings.py 수정
    
    * fix : 스태틱 경로
    
    * feat : cors 헤더
    
    * docs : requirements 수정
    
    * fix : static 경로 변경
    
    * feat : 제보 모델 추가, csv 로더 함수화
    
    * feat : 흡연구역 인포 윈도우 추가
    
    흡연구역 정확한 위치를 인포 윈도우에 나타나게끔 구현
    
    * feat : 로드뷰, 길찾기 기능 추가
    
    * Feat/map (#3)
    
    * fix : main.js 분기
    
    - main.js 분기
    
    * feat : navbar 로고 추가 및 하단 바 생성
    
    - navbar 로고 추가
    - navbar 아이콘 추가
    - 하단 바 생성
    
    * feat : 로드뷰 버튼 추가
    
    * feat : 흡연구역 마커 표시 기능
    
    * feat : 흡연구역 등록 버튼 추가
    
    * feat : 흡연 구역 표시 기능 추가
    
    * fix : 오류 수정
    
    * docs : requirements 수정
    
    * fix : error 메시지 추가
    
    * fix : settings.py 수정
    
    * fix : 스태틱 경로
    
    * feat : cors 헤더
    
    * docs : requirements 수정
    
    * fix : static 경로 변경
    
    * feat : 제보 모델 추가, csv 로더 함수화
    
    * feat : 흡연구역 인포 윈도우 추가
    
    흡연구역 정확한 위치를 인포 윈도우에 나타나게끔 구현
    
    * feat : 로드뷰, 길찾기 기능 추가
    
    * feat : 흡연 구역에 로드뷰 추가
    
    * docs : requirements 업데이트

--- main/models.py
+++ main/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class smokingZone(models.Model):
+    gu = models.CharField(max_length=10)
+    gubun = models.CharField(max_length=10)
+    type = models.CharField(max_length=10)
+    location = models.CharField(max_length=200)
+    manage = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.manage
+    
+class reportZone(models.Model):
+    gu = models.CharField(max_length=10)
+    gubun = models.CharField(max_length=10)
+    type = models.CharField(max_length=10)
+    location = models.CharField(max_length=200)
+    manage = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.manage
 
 class smoking_area(models.Model):
     region= models.CharField(max_length=100, null=True)
